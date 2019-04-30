f1a6ada097abf72e7711494d95fb2904e1bc9daa
r0ssini <48192508+r0ssini@users.noreply.github.com>
2019-04-30 16:51:51 -0500

    Add files via upload

--- insertEmp.php
+++ insertEmp.php
@@ -1,6 +1,8 @@
 <?php
 /*this file works in tandem with
-Gen_Emp_Update.html*/
+Gen_Emp_Update.html
+Change the <form> tag of the html page put
+<form action='insertEmp.php' method = "post"> */
 include("config.php");
 session_start();
 /*If db connection cannot be made
@@ -15,14 +17,17 @@ from html text boxes to post
 to the database*/
 $empFName = mysqli_real_escape_string($conn, $_POST['Emp_F_Name']);
 $empLName = mysqli_real_escape_string($conn, $_POST['Emp_L_Name']);
-$empNum   = mysqli_real_escape_string($conn, $_POST['Emp_Num']);
+$empNum   = mysqli_real_escape_string($conn, $_POST['Emp_#']);
 $empAddr  = mysqli_real_escape_string($conn, $_POST['Emp_Address']);
 $empRole  = mysqli_real_escape_string($conn, $_POST['Emp_Position']);
 
 /*This is an sql query to insert
 the values into the employee table*/
 $sql = "INSERT INTO employee ". "('employee_fname', 'employee_lname', 'employee_address', 'employee_role')". " VALUES ('$empFName', '$empLName', '$empNum', '$empAddr', '$empRole';";
-//currently cannot insert values table
+//CURRENTLY is not inserting values to table
+/*If time allows this will change to
+INSERT...ON DUPLICATE KEY UPDATE for both 
+new and updated entries*/
 
 
 //connects and submits the query to db
